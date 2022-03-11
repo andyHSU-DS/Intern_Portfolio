6b7668abbad48347c0c898b35e8323170a0313f7
andyHSU-DS <andy.hsu871226@gmail.com>
2022-03-11 11:17:58 +0800

    code

---
+++ 契約資料/main.py
@@ -0,0 +1,11 @@
+#載入套件
+import plotly
+import plotly.express as px
+import pandas as pd
+import plolty.graph_objects as go
+import plotly.io as pio
+pio.renders.default = 'notebook'
+from plotly.subplots import make_subplots
+import threading
+import sys
+import os
